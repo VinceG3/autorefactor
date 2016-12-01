9cc039b816c1e20cb67f76b50817894d11a3b9a9
Vincent Guidry <vguidry@tinroofsoftware.com>
2016-12-01 12:27:46 -0500

    .

--- lib/test.rb
+++ lib/test.rb
@@ -23,9 +23,9 @@ class Test
     require 'flammarion'
     f = Flammarion::Engraving.new
 
-    f.puts IO.read(@source)
+    f.pane.puts IO.read(@source)
     parsed = SourceFile.new(source).parse.inspect.uncolorize
-    f.puts parsed
+    f.pane.puts parsed
 
     f.wait_until_closed
     abort
