73d12da1477fc8082dcd5155389954ee33447f5d
Vincent Guidry <vguidry@tinroofsoftware.com>
2016-12-01 13:48:54 -0500

    .

--- lib/test.rb
+++ lib/test.rb
@@ -25,8 +25,8 @@ class Test
     left_pane.orientation = :horizontal
 
     left_pane.subpane('source-title').puts('<h2>Source:</h2>', raw: true)
-    left_pane.puts 'hi' # IO.read(@source)
-    parsed = SourceFile.new(source).parse.inspect.uncolorize
+    # left_pane.puts IO.read(@source)
+    # parsed = SourceFile.new(source).parse.inspect.uncolorize
     
     left_pane.pane(:right).subpane('parsed-title')
       .puts('<h2>Parsed:</h2>', raw: true)
