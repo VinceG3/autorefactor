274caa76bf48adf760d14079c370c9431d561d46
Vincent Guidry <vguidry@tinroofsoftware.com>
2016-12-10 17:26:32 -0500

    .

--- lib/develop.rb
+++ lib/develop.rb
@@ -9,6 +9,7 @@ class Develop
     left = $app.flow(width: 700)
     right = $app.flow(width: 700)
     left.para(source, font: 'Inconsolata')
-    right.para(source.each_char.to_a.first, font: 'Inconsolata')
+    next_char = source.each_char.to_a.first
+    right.para("next char: #{next_char}", font: 'Inconsolata')
   end
 end
