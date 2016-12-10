4fd4899b7e0048e92654231d142743cd5d4b82e4
Vincent Guidry <vguidry@tinroofsoftware.com>
2016-12-10 02:37:49 -0500

    .

--- lib/test.rb
+++ lib/test.rb
@@ -20,12 +20,13 @@ class Test
   end
 
   def develop_test
-    Shoes.app do
-      # title 'Source'
-      puts 'hi'
-      para 'hi'
-      para IO.read(@source)
-    end
+    puts IO.read(@source)
+    # Shoes.app do
+    #   # title 'Source'
+    #   puts 'hi'
+    #   para 'hi'
+    #   para IO.read(@source)
+    # end
   end
 
   def run_complete
