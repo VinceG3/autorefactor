c14672d79930d3a01b908cd33b6310bfcd44cc86
Vincent Guidry <vguidry@tinroofsoftware.com>
2016-12-10 23:25:29 -0500

    .

--- lib/develop.rb
+++ lib/develop.rb
@@ -15,44 +15,8 @@ class Develop
   def iterate
     # loop do
       display
-      load_parsers
-      @parsers.empty? ? pick_new_parser : pick_parser
+      Parser.load_all
+      @parsers.empty? ? Parser.pick_new : Parser.pick
     # end
-  end
-
-  def load_parsers
-    dir = Dir['./lib/project_types/*'].grep(Regexp.new(test.project_name)).first
-    files = Dir[File.join dir, '*.parser']
-    @parsers = files.collect{|f| Parser.load(f) }
-  end
-
-  def pick_new_parser
-    right.clear
-    right.para 'No parsers! Pick one:'
-    buttons = right.stack
-    buttons.button('Collector') { new_parser(:collector) }
-    buttons.button('Separator') { new_parser(:separator) }
-    buttons.button('Classifier') { new_parser(:classifier) }
-    buttons.button('Terminal') { new_parser(:terminal) }
-  end
-
-  def pick_parser
-    right.clear
-    parser_buttons = right.stack
-    parser_buttons.para 'Pick a parser or make a new one:'
-    @parsers.each do |parser|
-      parser_buttons.button(parser.name) { iterate_parser(parser) }
-    end
-    parser_buttons.button('New Parser') { pick_new_parser }
-  end
-
-  def iterate_parser(parser)
-    right.clear
-    right.para "Iterating #{parser.name}"
-  end
-
-  def new_parser(parser_type, name = nil)
-    name = $app.ask("Please name your new #{parser_type}") if name.nil?
-    Parser.create(name: name, type: parser_type, project: test.project_name)
   end
 end
--- lib/parser.rb
+++ lib/parser.rb
@@ -23,4 +23,40 @@ class Parser
   def filename
     File.join('.', 'lib', 'project_types', project, "#{name}.parser")
   end
+
+  def self.pick_new
+    right.clear
+    right.para 'No parsers! Pick one:'
+    buttons = right.stack
+    buttons.button('Collector') { new_parser(:collector) }
+    buttons.button('Separator') { new_parser(:separator) }
+    buttons.button('Classifier') { new_parser(:classifier) }
+    buttons.button('Terminal') { new_parser(:terminal) }
+  end
+
+  def self.pick
+    right.clear
+    parser_buttons = right.stack
+    parser_buttons.para 'Pick a parser or make a new one:'
+    @parsers.each do |parser|
+      parser_buttons.button(parser.name) { iterate_parser(parser) }
+    end
+    parser_buttons.button('New Parser') { pick_new_parser }
+  end
+
+  def iterate
+    right.clear
+    right.para "Iterating #{name}"
+  end
+
+  def self.load_all
+    dir = Dir['./lib/project_types/*'].grep(Regexp.new(test.project_name)).first
+    files = Dir[File.join dir, '*.parser']
+    @parsers = files.collect{|f| Parser.load(f) }
+  end
+
+  def self.make(parser_type, name = nil)
+    name = $app.ask("Please name your new #{parser_type}") if name.nil?
+    Parser.create(name: name, type: parser_type, project: test.project_name)
+  end
 end
