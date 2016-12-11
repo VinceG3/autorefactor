34abd97c3fb50cbf6274f0a48ba569c2a8ffb2bc
Vincent Guidry <vguidry@tinroofsoftware.com>
2016-12-10 21:20:47 -0500

    .

--- lib/develop.rb
+++ lib/develop.rb
@@ -1,14 +1,14 @@
 class Develop
-  attr_reader :test, :source
+  attr_reader :test, :source, :left, :right
   def initialize(test)
     @test = test
     @source = test.source
+    @left = $app.flow(width: 700)
+    @right = $app.flow(width: 700)
   end
 
   def display
     source_text = IO.read(source)
-    left = $app.flow(width: 700)
-    right = $app.flow(width: 700)
     left.para(source_text, font: 'Inconsolata')
     next_char = source_text.each_char.to_a.first
     right.para("next char: #{next_char}", font: 'Inconsolata')
@@ -21,7 +21,17 @@ class Develop
   end
 
   def load_parsers
-    dir = Dir['./lib/project_types/*'].grep(Regexp.new(test.project_name))
-    binding.pry
+    dir = Dir['./lib/project_types/*'].grep(Regexp.new(test.project_name)).first
+    files = Dir[File.join dir.first, '*.parser']
+    @parsers = files.collect{|f| Parser.load(f) }
+  end
+
+  def pick_new_parser
+    if @parsers.empty?
+      right.para 'No Parsers! Select one:'
+      button('Collector')
+      button('Separator')
+      button('Classifier')
+      button('Terminal')
   end
 end
