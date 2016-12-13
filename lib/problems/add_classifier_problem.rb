586cfc110919a33d660cfb17750d0e47e3b372c2
Vincent Guidry <vguidry@tinroofsoftware.com>
2016-12-13 17:47:07 -0500

    .

--- lib/problems/add_classifier_problem.rb
+++ lib/problems/add_classifier_problem.rb
@@ -1,12 +1,13 @@
 module Problems
   class AddClassifierProblem < Problem
     attr_reader :parser
-    
+
     def initialize(parser)
       @parser = parser
     end
 
     def fix
+      $app.alert('hi')
       $right.clear
       stack = $right.stack
       stack.para "Classifier needed for #{parser.name}"
