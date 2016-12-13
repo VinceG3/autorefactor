563a52fb682783d7bbb06fcc9a677d41e7744a56
Vincent Guidry <vguidry@tinroofsoftware.com>
2016-12-12 20:46:18 -0500

    .

--- lib/parser.rb
+++ lib/parser.rb
@@ -41,6 +41,7 @@ class Parser
     buttons.button('Separator') { make(project_type, :separator, is_first) }
     buttons.button('Classifier') { make(project_type, :classifier, is_first) }
     buttons.button('Terminal') { make(project_type, :terminal, is_first) }
+    make(project_type, :collector, is_first) if $no_shoes
   end
 
   # def self.pick
