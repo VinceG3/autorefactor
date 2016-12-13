425bfe4afe59663ec63a55b282f52f3f7a0b50fe
Vincent Guidry <vguidry@tinroofsoftware.com>
2016-12-13 00:06:22 -0500

    .

--- lib/initialize.rb
+++ lib/initialize.rb
@@ -5,19 +5,13 @@ require './lib/diff'
 require './lib/test'
 require './lib/develop'
 require './lib/parser'
+require './lib/problem'
 require './lib/parsing'
 require './lib/what_next'
 require './lib/ast_node'
 require './lib/source_file'
 require './lib/project_type'
 
-require './lib/try_parse/parse_machine'
-require './lib/try_parse/classifier'
-require './lib/try_parse/collector'
-require './lib/try_parse/separator'
-require './lib/try_parse/terminal'
-require './lib/try_parse/expression'
-require './lib/try_parse/sub_expression'
-require './lib/try_parse/parser'
+Dir['./lib/problems/*.rb'].each {|f| require f }
 Dir['./lib/try_parse/*.rb'].each {|f| require f }
 Dir['./lib/ruby/*.rb'].each {|f| require f }
